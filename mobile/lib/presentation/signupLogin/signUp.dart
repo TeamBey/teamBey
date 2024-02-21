3eed4fd33210945612c3a99a9d31b059695b5791
Sarra <sarrahammouda16@gmail.com>
2024-02-20 17:59:10 -0700

    User name, phone and password inputs been added and Sign up button created to the sign up interface

--- mobile/lib/presentation/signupLogin/signUp.dart
+++ mobile/lib/presentation/signupLogin/signUp.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:teambey_org/presentation/signupLogin/signIn.dart';
@@ -27,6 +28,7 @@ class _SignUpState extends State<SignUp> {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
                   padding: const EdgeInsets.only(top: 20),
@@ -50,10 +52,7 @@ class _SignUpState extends State<SignUp> {
                         ]
                       )),
                       SizedBox(width: width*0.18),
-                      Container(
-                        alignment: Alignment.topLeft,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                     Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text('Already have \nan account?',
@@ -69,11 +68,9 @@ class _SignUpState extends State<SignUp> {
                                 fontSize: 15,
                                 color: Colors.blue
                                ),)),
-                            
-                            
                           ],
                         ),
-                      ),
+                      
                     ],
                    ),
                 ),
@@ -102,21 +99,21 @@ class _SignUpState extends State<SignUp> {
                       borderRadius: BorderRadius.circular(10),
                       borderSide: BorderSide( 
                         color: Color.fromARGB(255, 33, 126, 115),
-                        width: width*0.005
+                        width: width*0.003
                       )
                     ),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
                       borderSide: BorderSide(
                         color:  Color.fromARGB(255, 33, 126, 115),
-                        width: width*0.005
+                        width: width*0.003
                       )
                     ),
                     focusedBorder: OutlineInputBorder( 
                       borderRadius: BorderRadius.circular(10),
                       borderSide: BorderSide( 
                         color: Color.fromARGB(255, 33, 126, 115),
-                        width: width*0.005
+                        width: width*0.003
                       )
                     ),
             
@@ -125,10 +122,182 @@ class _SignUpState extends State<SignUp> {
                ),
                SizedBox(height: height*0.03,),
                Row( 
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('User Name',
+                      style: GoogleFonts.cabin(
+                        fontSize: 18.5,
+                        color: Colors.black
+                      )),
+                      SizedBox(height: height*0.015,),
+                      Container(
+                        height: height*0.067,
+                        width: width*0.37,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextFormField(
+                          style: GoogleFonts.cabin(
+                            fontSize: 18
+                          ),
+                          decoration: InputDecoration( 
+                            hintText: 'User Name',
+                            hintStyle: GoogleFonts.cabin( 
+                              fontSize: 18,
+                              color: Colors.grey
+                            ),
+                            enabledBorder: OutlineInputBorder( 
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 33, 126, 115),
+                                width: width*0.003
+                              ),
+                            ),
+                            border: OutlineInputBorder( 
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide( 
+                                color: Color.fromARGB(255, 33, 126, 115),
+                                width: width*0.003
+                              )
+                            ),
+                            focusedBorder: OutlineInputBorder( 
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide( 
+                                color: Color.fromARGB(255, 33, 126, 115),
+                                width: width*0.003
+                              )
+                            )
+                          ),
+                          
+                        ),
+                      )
+
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Phone Number',
+                      style: GoogleFonts.cabin( 
+                        fontSize: 18.5,
+                        color: Colors.black
+                      ),),
+                      SizedBox(height: height*0.015,),
+                      Container( 
+                        height: height*0.067,
+                        width: width*0.37,
+                        decoration: BoxDecoration( 
+                          borderRadius: BorderRadius.circular( 10),
+                          
+                        ),
+                        child: TextFormField( 
+                           style: GoogleFonts.cabin( 
+                            fontSize: 18
+                           ),
+                           decoration: InputDecoration( 
+                            hintText: 'Phone Number',
+                            hintStyle: GoogleFonts.cabin( 
+                              fontSize: 18,
+                              color: Colors.grey
+                            ),
+                            border: OutlineInputBorder( 
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide( 
+                                color: Color.fromARGB(255, 33, 126, 115),
+                                width: width*0.003,
+                              )
+                            ),
+                            enabledBorder: OutlineInputBorder( 
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide( 
+                                color: Color.fromARGB(255, 33, 126, 115),
+                                width: width*0.003
+                              )
+                            ),
+                            focusedBorder: OutlineInputBorder( 
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide( 
+                                color: Color.fromARGB(255, 33, 126, 115),
+                                width: width*0.003
+                              )
+                            )
+
+                           ),
+                        ),
+                      )
+                    ],
+                  )
                 ],
-               )
+               ),
+               SizedBox(height: height*0.03,), 
+                  Text('Enter you Password',
+                  textAlign: TextAlign.left,
+                  style: GoogleFonts.cabin( 
+                    fontSize: 18,
+                    color: Colors.black
+                  ),),
+                SizedBox(height: height*0.015,),
+                Container(
+                  height: height*0.067,
+                  width: width*0.8,
+                 
+                  child: TextFormField( 
+                    style: GoogleFonts.cabin( 
+                      fontSize: 18
+                    ),
+                  decoration: InputDecoration( 
+                    hintText: 'Password',
+                    hintStyle: GoogleFonts.cabin( 
+                      fontSize: 18,
+                      color: Colors.grey
+                    ),
+                    border: OutlineInputBorder( 
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide( 
+                        color: Color.fromARGB(255, 33, 126, 115),
+                        width: width*0.003
+                      )
+                    ),
+                    enabledBorder: OutlineInputBorder( 
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide( 
+                        color: Color.fromARGB(255, 33, 126, 115),
+                        width: 0.003
+                      )
+                    ),
+                    focusedBorder: OutlineInputBorder( 
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide( 
+                        color: Color.fromARGB(255, 33, 126, 115),
+                        width: width*0.003
+                      )
+                    )
+                  ),
+                  ),
+                ),
+                SizedBox(height: height*0.035,),
+                Container(
+                  height: height*0.072,
+                  width: width*0.8,
+                  decoration: BoxDecoration( 
+                    color:Color.fromARGB(255, 33, 126, 115),
+                    borderRadius: BorderRadius.circular(15)
+                  ),
+                  child: CupertinoButton(child: Text('Sign Up',
+                  style: GoogleFonts.cabin( 
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white
+                  ),), 
+                  onPressed: (){
+                    
+                  }),
+                )
+
+               
               ],
             ),
           ),
